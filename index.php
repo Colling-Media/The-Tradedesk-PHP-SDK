26e4fdeb48b6df2b39f8c27f658f29fe5b0ae33a
Brian Logan <brianldj@gmail.com>
2016-04-18 16:16:49 -0700

    updated look for command line readability

--- index.php
+++ index.php
@@ -8,9 +8,10 @@ $partnerID = getenv("partnerID");
 
 $tradedesk = new Advertisers($username, $password);
 
-echo "<table width='100%'>";
-echo "<thead><tr><th>Advertiser</th><th>Description</th><th>Currency Code</th></tr></thead><tbody>";
 foreach($tradedesk->listAdvertisers($partnerID) AS $advertiser) {
-    echo "<tr><td>$advertiser->AdvertiserName</td><td>$advertiser->Description</td><td>$advertiser->CurrencyCode</td></tr>";
+    print_r("|---- ".$advertiser['AdvertiserName']." ----|\n");
+    print_r("|".$advertiser['Description']."|\n");
+    print_r("|".$advertiser['CurrencyCode']."|\n");
+    print_r("|--------|\n");
+    print_r("\n");
 }
-echo "</tbody></table>";
