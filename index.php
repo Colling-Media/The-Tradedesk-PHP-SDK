7eda30f44f8642c15a45073f67d30b117fcf1712
Brian Logan <brianldj@gmail.com>
2016-04-18 16:13:53 -0700

    updates to the testing, see if it looks better

--- index.php
+++ index.php
@@ -8,4 +8,9 @@ $partnerID = getenv("partnerID");
 
 $tradedesk = new Advertisers($username, $password);
 
-print_r($tradedesk->listAdvertisers($partnerID));
+echo "<table width='100%'>";
+echo "<thead><tr><th>Advertiser</th><th>Description</th><th>Currency Code</th></tr></thead><tbody>";
+foreach($tradedesk->listAdvertisers($partnerID) AS $advertiser) {
+    echo "<tr><td>$advertiser->AdvertiserName</td><td>$advertiser->Description</td><td>$advertiser->CurrencyCode</td></tr>";
+}
+echo "</tbody></table>";
